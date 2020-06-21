bbfd7a59bfbb7e9f9224a3f2f5cb1c232d61a6a2
Guilherme Avelino <guilhermeavelino1@outlook.com>
2020-06-21 12:21:11 -0300

    Introduzindo template engine

--- src/server.js
+++ src/server.js
@@ -1,4 +1,27 @@
 const express = require('express')
 const server = express()
 
+//Configuração de pasta pública
+server.use(express.static("public"))
+
+
+//Utilizando template engine
+const nunjucks = require('nunjucks')
+nunjucks.configure("src/views", {
+    express: server, //Ligando nunjucks ao express
+    noCache: true    //Não armazenar cache
+})
+
+//Configuração de rotas
+server.get('/', (req, res) => {
+    return res.render('index.html', {
+        title: "Seu marketplace de coleta de resíduos"
+    }) 
+})
+server.get('/create-point', (req, res) => {
+    return res.render('create-point.html') 
+})
+server.get('/search', (req, res) => {
+    return res.render('search-results.html') 
+})
 server.listen(3000)
